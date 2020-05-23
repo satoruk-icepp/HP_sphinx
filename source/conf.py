546b4e43014de7df8544aaee0418defa37b25f0c
Satoru Kobayashi <satoruk@icepp.s.u-tokyo.ac.jp>
2020-05-23 21:46:31 +0900

    Modification for Github pages

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,8 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   'sphinx.ext.mathjax'
+   'sphinx.ext.mathjax',
+   'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
